0013be61fad39c52ed4a9c23d750c7bbefc68fb1
Sandip Shrestha <shresthasandip1423@gmail.com>
2023-10-25 22:18:40 +0545

    Day 1

--- src/App.js
+++ src/App.js
@@ -1,7 +1,12 @@
 import React from "react";
+import Header from "./Header";
 
 function App() {
-  return <div>App</div>;
+  return (
+    <>
+      <Header />
+    </>
+  );
 }
 
 export default App;
---
+++ src/Header.js
@@ -0,0 +1,20 @@
+import React from "react";
+
+function Header() {
+  return (
+    <>
+      <div className="header">
+        <div className="title">Whaiyat!</div>
+        <div className="navbar">
+          <ul>
+            <li>Home</li>
+            <li>Somewhere</li>
+            <li>Ullu Swat</li>
+          </ul>
+        </div>
+      </div>
+    </>
+  );
+}
+
+export default Header;
